7b361b82c3805ba7ff64cda27b49ebf122da2a8f
Anishsuyal <anishsuyal28@gmail.com>
2025-06-30 19:49:00 +0530

    30 june

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,26 +1,130 @@
-import './App.css';
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import News from './components/News';
-import Newsitem from './components/Newsitem';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
-  pageSize=6
+  pageSize = 6;
+
+  state = {
+    progress: 0
+  };
+
+  setprogress = (progress) => {
+    this.setState({ progress: progress });
+  };
+
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            height={3}
+          />
+
           <Routes>
-            <Route path="/" element={<News key="general" pageSize={this.pageSize} country="us" category="general" />} />
-            <Route path="/business" element={<News key="business" pageSize={this.pageSize} country="us" category="business" />} />
-            <Route path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment" />} />
-            <Route path="/general" element={<News key="general" pageSize={this.pageSize} country="us" category="general" />} />
-            <Route path="/health" element={<News key="health" pageSize={this.pageSize} country="us" category="health" />} />
-            <Route path="/science" element={<News key="science" pageSize={this.pageSize} country="us" category="science" />} />
-            <Route path="/sports" element={<News key="sports" pageSize={this.pageSize} country="us" category="sports" />} />
-            <Route path="/technology" element={<News key="technology" pageSize={this.pageSize} country="us" category="technology" />} />
+            <Route
+              path="/"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="general"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              path="/business"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="business"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="entertainment"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              path="/general"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="general"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              path="/health"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="health"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              path="/science"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="science"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              path="/sports"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="sports"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              path="/technology"
+              element={
+                <News
+                  setprogress={this.setprogress}
+                  key="technology"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="technology"
+                />
+              }
+            />
           </Routes>
         </Router>
       </div>
--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -17,7 +17,7 @@ export class News extends Component {
     category: PropTypes.string,
   }
 
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       articles: [],
@@ -25,30 +25,36 @@ export class News extends Component {
       page: 1,
       totalResults: 0
     };
+    
   }
 
-  async componentDidMount() {
+  async componentDidMount(props) {
     this.fetchNews();
+    document.title=`${this.props.category}-NewsMonkey`;
   }
 
   fetchNews = async (page = this.state.page) => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=87502dbcc8ef4ccca83c7f9b5879cd43&page=${page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=87502dbcc8ef4ccca83c7f9b5879cd43&page=${page}&pageSize=${this.props.pageSize}`;
+  this.setState({ loading: true });
 
-    try {
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        articles: parsedData.articles,
-        loading: false,
-        totalResults: parsedData.totalResults,
-        page: page
-      });
-    } catch (error) {
-      console.error("Error fetching news:", error);
-      this.setState({ loading: false });
-    }
-  };
+  try {
+    this.props.setprogress(10);
+    let data = await fetch(url);
+    this.props.setprogress(30);
+    let parsedData = await data.json();
+    this.props.setprogress(50);
+    this.setState({
+      articles: parsedData.articles,
+      loading: false,
+      totalResults: parsedData.totalResults,
+      page: page
+    });
+    this.props.setprogress(100);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    this.setState({ loading: false });
+  }
+};
 
   handlenextclick = () => {
     if (this.state.page < Math.ceil(this.state.totalResults / this.props.pageSize)) {
